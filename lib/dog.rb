ab12a5f59285d7de44e613cab3df2fc666fe2fbf
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-02 02:09:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,5 +21,13 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS dogs
+      SQL
 
+     DB[:conn].execute(sql) 
+  end
+  
+  
 end
