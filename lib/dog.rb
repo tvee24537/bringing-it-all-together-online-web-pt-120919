b9d6e72fabcd7b15cb2b3ec242457200a0f5844b
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-02 02:12:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,8 +37,30 @@ class Dog
     dog
   end
 
+	  def self.create(name:, breed:)
+    dog = Dog.new(name, breed)
+    dog.save
+    dog
+  end
 
-	 def self.find_by_name(name)
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(result[0], result[1], result[2])
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+	
+	def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
       FROM dogs
